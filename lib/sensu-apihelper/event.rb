0e447239284438f8163b83e05bbee510596d2c21
Andy Sykes <github@tinycat.co.uk>
2013-03-17 01:57:48 +0000

    Add list all events

--- lib/sensu-apihelper/event.rb
+++ lib/sensu-apihelper/event.rb
@@ -7,6 +7,15 @@ module Sensu
 
       attr_accessor :client, :check, :occurrences, :output, :flapping, :status, :issued
 
+      def self.get_all
+        json = JSON.parse Net::HTTP.get URI("#{$api_url}/events")
+        if json != ""
+          json.map do |event_json|
+            Event.new(event_json)
+          end
+        end
+      end
+
       def self.get_by_client_and_check(client, check)
         json = Net::HTTP.get URI("#{$api_url}/events/#{client}/#{check}")
         if json != ""
@@ -20,10 +29,18 @@ module Sensu
         @client      = Client.get_by_name(json["client"])
         @check       = json["check"]
         @occurrences = json["occurrences"]
-        @output      = json["output"]
+        @output      = json["output"].chomp
         @flapping    = json["flapping"]
         @status      = json["status"]
         @issued      = json["issued"]
+      end
+
+      def inspect
+        "#{self.check} on #{self.client.name}: Output '#{output}', status #{status}, at #{issued}"
+      end
+
+      def to_s
+        inspect
       end
 
     end
